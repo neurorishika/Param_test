d2625e31a6de3834466d2a9f3d446415221a6e21
Saptarshi <saptarshi.mohanta@gmail.com>
2020-01-29 14:33:25 +0530

    commit from System

--- scripts/a5.sh
+++ scripts/a5.sh
@@ -11,5 +11,5 @@
 cd $SLURM_SUBMIT_DIR
 
 module load python/3.7
-python ../initExperiment.py '/home/collins/Param_test/Od/OdorA_High.odor' '/home/collins/Param_test/Op/Dur_6000_OdorDur_1000.protocol' '/home/collins/Param_test/Lc/Locust_A.locust' '5' $(date +%F_%H-%M)
+python /home/collins/Param_test/initExperiment.py '/home/collins/Param_test/Od/OdorA_High.odor' '/home/collins/Param_test/Op/Dur_6000_OdorDur_1000.protocol' '/home/collins/Param_test/Lc/Locust_A.locust' '5' $(date +%F_%H-%M)
 
