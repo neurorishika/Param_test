3823c97e257b817620fdb4acc161cfe8191f1e0e
Saptarshi <saptarshi.mohanta@gmail.com>
2020-03-04 15:39:35 +0530

    System

---
+++ scripts/lnpn_2.25_od_B_lc_B.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=48
+#SBATCH --time=12:00:00
+#SBATCH --job-name=tensorflow
+#SBATCH --error=out/job.%J.err
+#SBATCH --output=out/job.%J.out
+#SBATCH --partition=standard
+#SBATCH -a 0-9
+
+cd $SLURM_SUBMIT_DIR
+
+file="/home/collins/Param_test/label"
+name=$(cat "$file")
+name="${name}_2.25"
+
+module load python/3.7
+python /home/collins/Param_test/initExperiment.py '/home/collins/Param_test/Od/OdorB_High.odor' '/home/collins/Param_test/Op/Dur_6000_OdorDur_1000.protocol' '/home/collins/Param_test/Lc/Locust_B.locust' $SLURM_ARRAY_TASK_ID $name '2.25'
+
